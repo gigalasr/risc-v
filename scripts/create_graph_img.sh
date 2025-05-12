555622ea8a4a21282ac2e08922a41873b4f9f956
snh21 <lars.pfrenger@uni-ulm.de>
2025-05-12 18:01:58 +0000

    feat(readme): fix backgorund

--- scripts/create_graph_img.sh
+++ scripts/create_graph_img.sh
@@ -1,6 +1,6 @@
 GRAPH_RAW=$(tools/vhdlmake/build/vhdlmake graph)
 BASE64_DATA=$(echo "$GRAPH_RAW" | sed -n 's/.*#base64:\(.*\)$/\1/p')
 echo "$BASE64_DATA" | base64 -d |  jq -r '.code' > tmp.mmd
-mmdc -i tmp.mmd -o graph.svg -t dark
+mmdc -i tmp.mmd -o graph.svg -t dark --backgroundColor 020817
 mv graph.svg docs/graph.svg
 rm tmp.mmd
