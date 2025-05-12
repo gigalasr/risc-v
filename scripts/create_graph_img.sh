61988fe8531d187301edfaa1a9db7e428f6ddd41
snh21 <lars.pfrenger@uni-ulm.de>
2025-05-12 17:57:35 +0000

    feat(graph): dark mode

--- scripts/create_graph_img.sh
+++ scripts/create_graph_img.sh
@@ -1,6 +1,6 @@
 GRAPH_RAW=$(tools/vhdlmake/build/vhdlmake graph)
 BASE64_DATA=$(echo "$GRAPH_RAW" | sed -n 's/.*#base64:\(.*\)$/\1/p')
 echo "$BASE64_DATA" | base64 -d |  jq -r '.code' > tmp.mmd
-mmdc -i tmp.mmd -o graph.svg
+mmdc -i tmp.mmd -o graph.svg -t dark
 mv graph.svg docs/graph.svg
 rm tmp.mmd
