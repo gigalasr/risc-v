9327abc12f4171a24489d2121dd448574822fe12
snh21 <lars.pfrenger@uni-ulm.de>
2025-05-13 11:03:58 +0200

    fix: ci

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -6,5 +6,5 @@ find . -name "*_tb.vhdl" | while read -r filepath; do
     filename=$(basename $filepath)
     basename=${filename%$EXT}
     echo -e "\n\033[32mRunning testbench $basename \033[0m"
-    tools/vhdlmake/build/vhdlmake run $basename
+    tools/vhdlmake-bin run $basename
 done 
